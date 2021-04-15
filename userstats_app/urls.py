bd4829d4835a7fe3d1c9a522828246a5c20666d0
shivam <shivamchouhan728@gmail.com>
2021-04-15 15:51:33 +0530

    👷 add income sources summary APIView

--- userstats_app/urls.py
+++ userstats_app/urls.py
@@ -1,7 +1,8 @@
-from .views import ExpenseSummaryStats
+from .views import ExpenseSummaryStats, IncomeSourcesSummaryStats
 from django.urls import path
 
 
 urlpatterns = [
-    path('expense-category-data', ExpenseSummaryStats.as_view(), name="expense_category_summary")
+    path('expense-category-data', ExpenseSummaryStats.as_view(), name="expense_category_summary"),
+    path('income-sources-data', IncomeSourcesSummaryStats.as_view(), name="income_sources_summary"),
 ]
--- userstats_app/views.py
+++ userstats_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 import datetime
 from expense_app.models import Expense
+from income_app.models import Income
 from rest_framework import status, response
 
 # Create your views here.
@@ -42,3 +43,36 @@ class ExpenseSummaryStats(APIView):
                 )
 
         return response.Response({'category_data': final}, status=status.HTTP_200_OK)
+
+
+class IncomeSourcesSummaryStats(APIView):
+
+    def get_amount_for_source(self, income_list, source):
+        income = income_list.filter(source=source)
+        amount = 0
+
+        for i in income:
+            amount += i.amount
+
+        return {'amount':str(amount)}
+
+    def get_source(self, income):
+        """return sources of income"""
+        return income.source
+
+    def get(self, request):
+        todays_date = datetime.date.today()
+        a_year_ago = todays_date - datetime.timedelta(days=30 * 12)
+        income = Income.objects.filter(
+            owner=request.user, date__gte=a_year_ago, date__lte=todays_date)  #date__gte => greater or equal
+
+        final = {}
+        sources = list(set(map(self.get_source, income)))
+
+        for i in income:
+            for source in sources:
+                final[source] = self.get_amount_for_source(
+                    income, source
+                )
+
+        return response.Response({'income-source-data': final}, status=status.HTTP_200_OK)
